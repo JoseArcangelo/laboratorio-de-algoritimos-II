3e46c5739ffc17988bd33737069aa43625655cdf
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-08-18 15:08:11 -0300

    Update exercicio06.py

--- semana02/exercicio06.py
+++ semana02/exercicio06.py
@@ -4,12 +4,17 @@ matriz = [
     [7,  8,  9],
 ]
 resposta = True
+parar = False
             
 for linha in range(len(matriz)):
+    if parar == True:
+        break
     for elemento in range(len(matriz[linha])):
         if matriz[linha][elemento] != matriz[elemento][linha] or len(matriz) != len(matriz[linha]):
             resposta = False
+            parar = True
             break
         else:
             resposta = True
+    
 print(resposta)
