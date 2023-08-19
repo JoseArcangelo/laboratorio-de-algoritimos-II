7fe5dde6df84ad62f08ffdb496034ff97263e907
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-08-19 19:25:41 -0300

    Update exercicio01.py

--- semana02/exercicio01.py
+++ semana02/exercicio01.py
@@ -1,10 +1,15 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(lista)
-l2 = []
-contador = len(lista)
-
-for i in range(len(lista)):
-    contador = contador - 1
-    l2.append(lista[contador])
+def inverter_lista(l1, l2):
+    contador = len(l1)
+    for i in range(len(l1)):
+        contador = contador - 1
+        l2.append(l1[contador])
+    return l2
     
-print(l2)
+def main():
+    l1 = [0 ,1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(l1)
+    l2 = []
+    l2 = inverter_lista(l1, l2)
+    print(l2)
+    
+main()
