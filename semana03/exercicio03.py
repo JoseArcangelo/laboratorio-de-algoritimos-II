c3d2b9ef78e8cb3ad6ab142907209b8519311cc0
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-08-24 17:46:49 -0300

    Create exercicio03.py

---
+++ semana03/exercicio03.py
@@ -0,0 +1,29 @@
+def dinheiro_entregar(saque):
+    cedulas = []
+    while saque != 0:
+        if saque >= 100:
+            cedulas.append(100)
+            saque -= 100
+        elif saque >= 50:
+            cedulas.append(50)
+            saque -= 50
+        elif saque >= 20:
+            cedulas.append(20)
+            saque -= 20
+        elif saque >= 10:
+            cedulas.append(10)
+            saque -= 10
+            
+    print("Cedulas a serem recebidas:")
+    print(cedulas.count(100), "de R$", 100)
+    print(cedulas.count(50), "de R$", 50)
+    print(cedulas.count(20), "de R$", 20)
+    print(cedulas.count(10), "de R$", 10)
+
+def main():
+    saque = int(input("Informe o valor do saque:"))
+    if saque % 10 == 0:
+        dinheiro_entregar(saque)
+    else:
+        print("Valor inválido!")
+main()
