b1280f34f8d56f0e6d724a380a3c52f187496549
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-08-25 10:38:43 -0300

    Create exercicio02.py

---
+++ semana03/exercicio02.py
@@ -0,0 +1,16 @@
+matriz = [
+    [1, 2, 3, 4, 5],
+    [1, 1, 1, 4, 5],
+    [9, 2, 7, 4, 5],
+    [2, 2, 3, 5, 5],
+    [1, 2, 6, 4, 6]
+    ]
+
+soma = 0
+for line in range(len(matriz)):
+    for colunm in range(len(matriz[line])):
+        if line != colunm and line - colunm > 0:
+            soma += matriz[line][colunm]
+            
+print("Resultado da soma:", soma)
+        
