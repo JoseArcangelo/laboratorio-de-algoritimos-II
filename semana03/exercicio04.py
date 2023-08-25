89b407ed4227d06416e3afc1a7f764868ddf6d81
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-08-25 19:12:52 -0300

    Update exercicio04.py

--- semana03/exercicio04.py
+++ semana03/exercicio04.py
@@ -1,12 +1,13 @@
 largest_value = [0, 0]
 matriz = [
-    [1, 2, 3, 4, 5],
-    [1, 1, 1, 4, 5],
-    [9, 2, 7, 4, 5],
-    [2, 2, 3, 5, 5],
-    [1, 2, 6, 4, 6]
-    ]
-diagonal = 1    
+    [1, 1, 1, 1, 1],
+    [1, 2, 1, 1, 1],
+    [1, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+]
+
+diagonal = 1
 for line in range(len(matriz)):
     horizontal = 1
     vertical = 1
@@ -15,17 +16,17 @@ for line in range(len(matriz)):
         if horizontal > largest_value[0]:
             largest_value[0] = horizontal
             largest_value[1] = f"horizontal: {line}"
-                
+
         vertical *= matriz[column][line]
         if vertical > largest_value[0]:
             largest_value[0] = vertical
-            largest_value[1] = f"vertical: {column}"
-            
+            largest_value[1] = f"vertical: {line}"
+
         if column == line:
             diagonal *= matriz[line][column]
             if diagonal > largest_value[0]:
                 largest_value[0] = diagonal
                 largest_value[1] = "Diagonal principal"
-                
-print("O maior valor é da", largest_value[1], "que resultou em",largest_value[0])
+
+print("O maior valor é da", largest_value[1], "que resultou em", largest_value[0])
 
