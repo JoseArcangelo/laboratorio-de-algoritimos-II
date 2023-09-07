df2385602238d7b18b33ba2d4d4fbfabece06d22
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-09-07 13:26:44 -0300

    Update TRABALHO.py

--- semana05/TRABALHO.py
+++ semana05/TRABALHO.py
@@ -10,39 +10,57 @@ def menu():
 
 def add_product(products):
     name = input("Informe o nome do produto: ")
-    description = {}
-    description["amount"] = int(input(f"Informe a quantidade do estoque do produto {name}: "))
-    description["price"] = float(input(f"Informe o preço unitário do produto {name}: "))
-    products[name] = description
+    amount = int(input(f"Informe a quantidade que deseja adicionar ao estoque de {name}: "))
+    if name in products:
+        products[name]["amount"] += amount
+        
+    else:
+        description = {}
+        description["amount"] = amount
+        description["price"] = float(input(f"Informe o preço unitário do produto {name}: "))
+        products[name] = description
+    print("--PRODUTO ADICIONADO E INCLUIDO NO ESTOQUE!--")
     return products
 
 def search_product(products):
+    print("\n--BUSCAR PRODUTO--")
     name = input("Informe o nome do produto que deseja encontrar: ")
     if name in products:
-        print(f"Informações do produto --> Estoque: {products[name].get('amount')} unidades - Preço: R$ {products[name].get('price')} \n")
+        print(f"Informações do produto --> Nome: {name} - Estoque: {products[name].get('amount')} unidades - Preço: R$ {products[name].get('price')} \n")
     
     else:
-        print("ESTE PRODUTO NÃO CONSTA EM NOSSO SISTEMA!")
+        print("--ESTE PRODUTO NÃO CONSTA EM NOSSO SISTEMA!--")
 
 def show_products(products):
-    print("----LISTA DE PRODUTOS----")
+    print("\n----LISTA DE PRODUTOS----")
     for product in products:
-        print(f"->Produto:{product} Quantidade em estoque: {products[product].get('amount')} unidades - Preço: R$ {products[product].get('price')} \n")
+        print(f"->Produto:{product} Quantidade em estoque: {products[product].get('amount')} unidades - Preço: R$ {products[product].get('price')}\n")
 
 
 def sell_produts(products, sales_record):
-    print("----VENDER PRODUTO----\n")
+    print("\n----VENDER PRODUTO----")
+    
     name = input("Informe o nome do produto vendido: ")
     if name in products:
-        amount = int(input(f"Informe a quantidade de {name} vendidos: "))
+        quantity_sell = int(input(f"Informe a quantidade de {name} vendidos: "))
 
-        if amount > products[name].get('amount'):
+        if quantity_sell > products[name].get('amount'):
             print("NOSSO ESTOQUE É INFERIOR A QUANTIDADE QUE DESEJA VENDEER!")
         
         else:
-            products[name]
+            products[name]["amount"] -= quantity_sell
+            lucro = quantity_sell * products[name]["price"]
+            if name not in sales_record:
+                detals = {}
+                detals["lucro"] = lucro
+                detals["amount"] = quantity_sell
+                sales_record[name] = detals
+                
+    else:
+        print("OPS! NÃO POSSUIMOS ESTE PRODUTO!")
         
-            
+    return products, sales_record
+        
 
 def main():
     print("---<BEM VINDO AO MERCADINHO DO PILÉCO>---")
@@ -62,6 +80,6 @@ def main():
         
         elif option == 4:
             sell_produts(products, sales_record)
+            print(sales_record, products)
             
-
 main()
