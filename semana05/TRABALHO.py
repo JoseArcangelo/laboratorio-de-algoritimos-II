09b35b7e08407adbe0f2d58f55fb50e7bc147240
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-09-13 09:54:21 -0300

    Update TRABALHO.py

--- semana05/TRABALHO.py
+++ semana05/TRABALHO.py
@@ -1,85 +1,111 @@
 def menu():
-    print("1 - Adicionar um produto")
+    print("\n1 - Adicionar um produto")
     print("2 - Buscar um produto")
     print("3 - Visualizar todos os produtos")
     print("4 - Vender um produto")
     print("5 - Relatorio de vendas")
     print("6 - Sair")
-    option = int(input("Informe a opcao desejada: "))
+    option = input("Informe a opcao desejada: ")
     return option
 
-def add_product(products):
+def add_product(my_products):
+    print("\n --ADICIONAR PRODUTO--")
     name = input("Informe o nome do produto: ")
-    amount = int(input(f"Informe a quantidade que deseja adicionar ao estoque de {name}: "))
-    if name in products:
-        products[name]["amount"] += amount
-        
-    else:
-        description = {}
-        description["amount"] = amount
-        description["price"] = float(input(f"Informe o preço unitário do produto {name}: "))
-        products[name] = description
-    print("--PRODUTO ADICIONADO E INCLUIDO NO ESTOQUE!--")
-    return products
+    amount = int(input(f"Informe a quantidade que deseja adicionar ao estoque de {name}: ")
 
-def search_product(products):
+    if name in my_products:
+        my_products[name]["amount"] += amount
+
+    else:
+        description = {
+            "amount": amount,
+            "price": float(input(f"Informe o preço unitário do produto {name}:")),
+        }
+        my_products[name] = description
+
+    print("--PRODUTO ADICIONADO!--")
+    return my_products
+
+def search_product(my_products):
     print("\n--BUSCAR PRODUTO--")
     name = input("Informe o nome do produto que deseja encontrar: ")
-    if name in products:
-        print(f"Informações do produto --> Nome: {name} - Estoque: {products[name].get('amount')} unidades - Preço: R$ {products[name].get('price')} \n")
-    
+    if name in my_products:
+        print(f"Informações do produto --> Nome: {name} - Estoque: {my_products[name]['amount']} unidades - Preço: R$ {my_products[name]['price']} \n")
+
     else:
         print("--ESTE PRODUTO NÃO CONSTA EM NOSSO SISTEMA!--")
 
-def show_products(products):
+def show_products(my_products):
     print("\n----LISTA DE PRODUTOS----")
-    for product in products:
-        print(f"->Produto:{product} Quantidade em estoque: {products[product].get('amount')} unidades - Preço: R$ {products[product].get('price')}\n")
+    for product in my_products:
+        print(f"-->Produto: {product} - Quantidade em estoque: {my_products[product]['amount']} unidades - Preço: R$ {my_products[product]['price']}\n")
 
-
-def sell_produts(products, sales_record):
+def sell_produts(my_products, sales_record):
     print("\n----VENDER PRODUTO----")
-    
+
     name = input("Informe o nome do produto vendido: ")
-    if name in products:
+    if name in my_products:
         quantity_sell = int(input(f"Informe a quantidade de {name} vendidos: "))
 
-        if quantity_sell > products[name].get('amount'):
-            print("NOSSO ESTOQUE É INFERIOR A QUANTIDADE QUE DESEJA VENDEER!")
-        
+        if quantity_sell > my_products[name]["amount"]:
+            print("NOSSO ESTOQUE É INFERIOR A QUANTIDADE QUE DESEJA VENDER!")
+
         else:
-            products[name]["amount"] -= quantity_sell
-            lucro = quantity_sell * products[name]["price"]
+            my_products[name]["amount"] -= quantity_sell
+            profit_sale = quantity_sell * my_products[name]["price"]
+
             if name not in sales_record:
                 detals = {}
-                detals["lucro"] = lucro
+                detals["sale"] = profit_sale
                 detals["amount"] = quantity_sell
                 sales_record[name] = detals
-                
+
+            else:
+                sales_record[name]["sale"] += profit_sale
+                sales_record[name]["amount"] += quantity_sell
+
+            if my_products[name]["amount"] == 0:
+                my_products.pop(name)
+
     else:
         print("OPS! NÃO POSSUIMOS ESTE PRODUTO!")
-        
-    return products, sales_record
-        
+
+    return my_products, sales_record
+
+def sales(sales_record):
+    print("\n--RELATÓRIO DE VENDAS--")
+    for product in sales_record:
+        print(f"-> Nome do produto: {product} - Quantidade vendida: {sales_record[product]['amount']} unidade - Lucro total: R$ {sales_record[product]['sale']}")
 
 def main():
     print("---<BEM VINDO AO MERCADINHO DO PILÉCO>---")
-    products = {}
+    my_products = {}
     sales_record = {}
 
     while True:
         option = menu()
-        if option == 1:
-            add_product(products)
+        if option == "1":
+            my_products = add_product(my_products)
 
-        elif option == 2:
-            search_product(products)
+        elif option == "2":
+            search_product(my_products)
 
-        elif option == 3:
-            show_products(products)
-        
-        elif option == 4:
-            sell_produts(products, sales_record)
-            print(sales_record, products)
-            
+        elif option == "3":
+            show_products(my_products)
+
+        elif option == "4":
+            my_products, sales_record = sell_produts(my_products, sales_record)
+
+        elif option == "5":
+            sales(sales_record)
+
+        elif option == "6":
+            print("VOLTE SEMPRE AO MERCADINHO DO PILÉCO!!")
+            print("Saindo...")
+            break
+
+        else:
+            print("--OPÇÃO INVÁLIDA!!!--")
+
 main()
+
