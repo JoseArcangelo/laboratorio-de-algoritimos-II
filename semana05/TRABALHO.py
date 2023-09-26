248be7d2dd6668786369ec7ba53ab6fb970006ad
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-09-26 16:57:18 -0300

    Update TRABALHO.py

--- semana05/TRABALHO.py
+++ semana05/TRABALHO.py
@@ -16,19 +16,19 @@ def menu():
 
 def add_product(stock, historical_changes):
     print("\n --ADICIONAR PRODUTO--")
-    name = input("Informe o nome do produto: ")
+    name_product = input("Informe o nome do produto: ")
     amount = int(
-        input(f"Informe a quantidade que deseja adicionar ao estoque de {name}: "))
-    if name in stock:
-        stock[name]["amount"] += amount
-        historical_changes.append(f"Foi adicionado {amount} unidades ao estoque de {name}")
+        input(f"Informe a quantidade que deseja adicionar ao estoque de {name_product}: "))
+    if name_product in stock:
+        stock[name_product]["amount"] += amount
+        historical_changes.append(f"Foi adicionado {amount} unidades ao estoque de {name_product}")
     else:
-        price = float(input(f"Informe o preço unitário de {name}: "))
-        stock[name] = {
+        price = float(input(f"Informe o preço unitário de {name_product}: "))
+        stock[name_product] = {
             "amount": amount,
             "price": price,
             "price_historic": [price],
-            "category": input(f"Informe a categoria de {name}: "),}
+            "category": input(f"Informe a categoria de {name_product}: "),}
     print("--PRODUTO ADICIONADO!--")
     return stock, historical_changes
 
@@ -54,21 +54,21 @@ def show_products(stock):
 
 def sell_produts(stock, sales_record):
     print("\n----VENDER PRODUTO----")
-    name = input("Informe o nome do produto vendido: ")
-    if name in stock:
-        quantity_sell = int(input(f"Informe a quantidade de {name} vendidos: "))
-        if quantity_sell > stock[name]["amount"]:
+    name_product = input("Informe o nome do produto vendido: ")
+    if name_product in stock:
+        quantity_sell = int(input(f"Informe a quantidade de {name_product} vendidos: "))
+        if quantity_sell > stock[name_product]["amount"]:
             print("NOSSO ESTOQUE É INFERIOR A QUANTIDADE QUE DESEJA VENDER!")
         else:
-            stock[name]["amount"] -= quantity_sell
-            profit_sale = quantity_sell * stock[name]["price_historic"][-1]
+            stock[name_product]["amount"] -= quantity_sell
+            profit_sale = quantity_sell * stock[name_product]["price_historic"][-1]
             print("Valor total da venda: R$", profit_sale)
             date_now = datetime.datetime.now()
             date_now = f"{date_now.year}/{date_now.month}/{date_now.day} {date_now.hour} horas {date_now.minute} min"
-            sales_record.append(f"Nome do produto: {name} - Quantidade vendida: {quantity_sell} - Lucro da venda: R$ {profit_sale} - Data: {date_now}")
+            sales_record.append(f"Nome do produto: {name_product} - Quantidade vendida: {quantity_sell} - Lucro da venda: R$ {profit_sale} - Data: {date_now}")
 
-            if stock[name]["amount"] == 0:
-                stock.pop(name)
+            if stock[name_product]["amount"] == 0:
+                stock.pop(name_product)
     else:
         print("OPS! NÃO POSSUIMOS ESTE PRODUTO!")
     return stock, sales_record
@@ -80,27 +80,27 @@ def sales(sales_record):
 
 def delete_product(stock, historical_changes):
     print("\n --ECLUIR PRODUTO--")
-    name = input("Informe o nome do produto que deseja remover de seu estoque: ")
-    if name in stock:
-        stock.pop(name)
-        print(f"{name} foi removido com sucesso!")
+    name_product = input("Informe o nome do produto que deseja remover de seu estoque: ")
+    if name_product in stock:
+        stock.pop(name_product)
+        print(f"{name_product} foi removido com sucesso!")
         date_now = datetime.datetime.now()
         date_now = f"{date_now.year}/{date_now.month}/{date_now.day} {date_now.hour} horas {date_now.minute} min e {date_now.second} seg"
-        historical_changes.append(f"O produto {name} foi removido do stock - DATA: {date_now}")
+        historical_changes.append(f"O produto {name_product} foi removido do stock - DATA: {date_now}")
     else:
         print("OPS! NÃO POSSUIMOS ESTE PRODUTO!")
     return stock, historical_changes
 
 def change_product_value(stock, historical_changes):
     print("\n--ALTERAR VALOR DE UM PRODUTO--")
-    name = input("Informe o nome do produto que deseja alterar o valor: ")
-    if name in stock:
-        new_value = float(input(f"Informe o valor atualizado de {name}: "))
+    name_product = input("Informe o nome do produto que deseja alterar o valor: ")
+    if name_product in stock:
+        new_value = float(input(f"Informe o valor atualizado de {name_product}: "))
         date_now = datetime.datetime.now()
         date_now = f"{date_now.year}/{date_now.month}/{date_now.day} {date_now.hour} horas {date_now.minute} min"
-        historical_changes.append(f"O preço unitáro de {name} que era R$ {stock[name]['price_historic'][-1]} for alterado para R$ {new_value} data: {date_now} ")
-        stock[name]["price_historic"].append(new_value)
-        stock[name]["price"] = new_value
+        historical_changes.append(f"O preço unitáro de {name_product} que era R$ {stock[name_product]['price_historic'][-1]} for alterado para R$ {new_value} data: {date_now} ")
+        stock[name_product]["price_historic"].append(new_value)
+        stock[name_product]["price"] = new_value
         print("O PREÇO DO PRODUTO FOI ALTERADO!")
 
     else:
