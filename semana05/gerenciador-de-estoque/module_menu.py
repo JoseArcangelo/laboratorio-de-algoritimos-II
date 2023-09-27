2c4533a88425397645798e39e1475b54bbaf2e30
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-09-27 14:10:24 -0300

    Update module_menu.py

--- semana05/gerenciador-de-estoque/module_menu.py
+++ semana05/gerenciador-de-estoque/module_menu.py
@@ -1,14 +1,16 @@
 def menu():
+    """Função que exibe o menu de opções."""
+    
     print("\n---< MENU >---")
     print("1 - Adicionar um produto")
     print("2 - Buscar um produto")
     print("3 - Visualizar todos os produtos")
     print("4 - Vender um produto")
-    print("5 - Relatorio de vendas")
+    print("5 - Relatório de vendas")
     print("6 - Excluir produto")
     print("7 - Alterar valor de um produto")
-    print("8 - Visualizar po categoria")
-    print("9 - Visualizar historico de alterações")
+    print("8 - Visualizar por categoria")
+    print("9 - Visualizar histórico de alterações")
     print("10 - Sair")
     option = input("Informe a opcao desejada: ")
     return option
