aec72bcdde2b4ad121ca066b95e42d41ce37699c
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-09-27 08:44:10 -0300

    Create module_menu.py

---
+++ semana05/gerenciador-de-estoque/module_menu.py
@@ -0,0 +1,14 @@
+def menu():
+    print("\n---< MENU >---")
+    print("1 - Adicionar um produto")
+    print("2 - Buscar um produto")
+    print("3 - Visualizar todos os produtos")
+    print("4 - Vender um produto")
+    print("5 - Relatorio de vendas")
+    print("6 - Excluir produto")
+    print("7 - Alterar valor de um produto")
+    print("8 - Visualizar po categoria")
+    print("9 - Visualizar historico de alterações")
+    print("10 - Sair")
+    option = input("Informe a opcao desejada: ")
+    return option
