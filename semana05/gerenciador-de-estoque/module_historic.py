7760e3bd9eb4a456a7b2b4ca99fb744e8854d7b5
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-09-27 15:19:42 -0300

    Update module_historic.py

--- semana05/gerenciador-de-estoque/module_historic.py
+++ semana05/gerenciador-de-estoque/module_historic.py
@@ -1,22 +1,23 @@
-def sales(sales_record):
+def see_sales_reports(sales_record):
     """Função que lista todas as vendas realizadas,
     ela recebe como parametros o historico de vendas."""
 
     print("\n--RELATÓRIO DE VENDAS--")
     if len(sales_record) == 0:
-        print("OPS! Nenhuma venda foi realizada")
+        print("OPS! Nenhuma venda foi realizada!")
     else: 
         for sale in sales_record:
             print(sale)
 
 def see_historical_change(historical_changes):
-    """Função que lista todas as alterações no estoque, incluindoatualizações, adições 
+    """Função que lista todas as alterações no estoque, incluindo atualizações, adições 
     e exclusões, ela recebe como parametros o historico de alterações."""
 
     print("--\nHistórico de alterações--")
     if len(historical_changes) == 0:
-        print("OPS! Nenhuma venda foi realizada")
+        print("OPS! Nenhuma alteração foi feita!")
     else:
         for historic in historical_changes:
             print(historic)
+
 
