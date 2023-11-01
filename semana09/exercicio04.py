e6825c94990a7efa5a8c4ecae1f83515113c331e
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-11-01 19:58:56 -0300

    Update exercicio04.py

--- semana09/exercicio04.py
+++ semana09/exercicio04.py
@@ -1,17 +1,23 @@
 def find_element(lista, indice):
-  try:
-    if indice > len(lista) - 1 or indice < 0:
-      raise IndexError("Valor inválido")
-    return lista[indice]
+    try:
+        if indice > len(lista) - 1 or indice < 0:
+            raise IndexError("Valor inválido")
+        return lista[indice]
     
-  except IndexError as e:
-    print(e)
+    except IndexError as e:
+        print(e)
 
 lista = ["a", "b", "c", "d", "e"]
-try:
-  indice = int(input("Informe o indice: "))
-except ValueError:
-  print("Valor inválido")
+
+while True:
+    try:
+        indice = int(input("Informe o indice: "))
+        break
+    except ValueError:
+        print("Valor inválido")
+    except BaseException:
+        print("[ERRO]")
+
   
 elemento = find_element(lista, indice)
 if elemento != None:
