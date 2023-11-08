7996c5f8acb86fea82340f36a1d1d93f5334bf2d
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-11-08 14:21:19 -0300

    Update exercicio01.py

--- semana10./exercicio01.py
+++ semana10./exercicio01.py
@@ -1,6 +1,6 @@
 import random
 
-def criar_arquivo(amount):
+def create_file(amount):
     file = open('number.txt', 'w')
 
     for i in range(amount):
@@ -9,21 +9,22 @@ def criar_arquivo(amount):
 
     file.close()
         
-def ler_numbers():
-    file = open('number.txt', 'r')
+def calculate_average():
+    file = open('exercicio01/number.txt', 'r')
     line_lines = file.readlines()
-    line_lines = line_lines[0]
-    print(line_lines)
-   
-    
     file.close()
+    line_lines = line_lines[0]
+    line_lines = line_lines.split(",")
+    average = 0
 
-
-
-
-
+    for number in line_lines:
+        average += int(number)
+    average = average / len(line_lines)
+    return average
+   
 def main():
-    criar_arquivo(20)
-    ler_numbers()
+    create_file(5)
+    average = calculate_average()
+    print("A media destes numeros no arquivo é ", average)
 
 main()
