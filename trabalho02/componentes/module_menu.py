a9a1c0f6d34ce567353bc2aa9adfce16a8d5dfe2
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-11-13 17:00:58 -0300

    Create module_menu.py

---
+++ trabalho02/componentes/module_menu.py
@@ -0,0 +1,8 @@
+def menu():
+    """Menu de opções do jogo"""
+    print("\n1 - UMA PALAVRA")
+    print("2 - DUETO")
+    print("3 - QUARTETO")
+    print("4 - SAIR")
+    option = input("INFORME UMA OPÇÃO DE JOGO: ")
+    return option
