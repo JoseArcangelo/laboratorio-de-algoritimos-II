7225168e2e7e926a57c64c4ca20c38081a388f7e
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-11-15 23:13:27 -0300

    Update module_game.py

--- trabalho02/componentes/module_game.py
+++ trabalho02/componentes/module_game.py
@@ -36,17 +36,18 @@ def input_word_tentativa():
          print(e)
 
 def colorir_palavras(round, letras_verdes, letras_amarelas, historico_palavras_tentadas, palavra, palavra_tentativa, letra , palavras_sorteadas):
-   
    for i in range(5):
       lista_indice_letras_verdes = list(letras_verdes.values())
-      lista_indices = list(letras_verdes.keys())
-
+      lista_indices = list(letras_verdes.keys()) 
+      
       if i in lista_indices:
          colored_letter = f"\033[32m{palavra_tentativa[i]}\033[0m"
          historico_palavras_tentadas[round][palavra] = f"{historico_palavras_tentadas[round][palavra]}{colored_letter} | "
+      
       elif i in letras_amarelas and lista_indice_letras_verdes.count(palavra_tentativa[i]) != palavras_sorteadas[palavra].count(palavra_tentativa[i]):
          colored_letter = f"\033[33m{palavra_tentativa[i]}\033[0m"
          historico_palavras_tentadas[round][palavra] = f"{historico_palavras_tentadas[round][palavra]}{colored_letter} | "
+     
       else:
          historico_palavras_tentadas[round][palavra] = f"{historico_palavras_tentadas[round][palavra]}{palavra_tentativa[i]} | "
    return historico_palavras_tentadas
@@ -70,26 +71,43 @@ def game(total_words):
       palavra_tentativa = input_word_tentativa()
       module_utilits.clear_terminal()
       
-      letras_verdes = {}
-      letras_amarelas = []
+      
       for palavra in range(len(palavras_sorteadas)):
          historico_palavras_tentadas[round].append("")
-         for letra in range(len(palavras_sorteadas[palavra])):
-            if palavra_tentativa[letra] == palavras_sorteadas[palavra][letra]:
-               letras_verdes[letra] = palavra_tentativa[letra]
+         letras_verdes = {}
+         letras_amarelas = []
+         if acertos[palavra] == True:
+            historico_palavras_tentadas[round][palavra] = historico_palavras_tentadas[round-1][palavra]
+         else:
+            for letra in range(len(palavras_sorteadas[palavra])):
+               if palavra_tentativa[letra] == palavras_sorteadas[palavra][letra]:
+                  letras_verdes[letra] = palavra_tentativa[letra]
 
-            elif palavra_tentativa[letra] in palavras_sorteadas[palavra]:
-               letras_amarelas.append(letra)
+               elif palavra_tentativa[letra] in palavras_sorteadas[palavra]:
+                  letras_amarelas.append(letra)
+               
+               if palavra_tentativa == palavras_sorteadas[palavra]:
+                  acertos[palavra] = True
+         
+            historico_palavras_tentadas = colorir_palavras(round, letras_verdes, letras_amarelas, historico_palavras_tentadas, palavra, palavra_tentativa, letra, palavras_sorteadas)
       
-         historico_palavras_tentadas = colorir_palavras(round, letras_verdes, letras_amarelas, historico_palavras_tentadas, palavra, palavra_tentativa, letra, palavras_sorteadas)
+      for i in range(len(historico_palavras_tentadas)):
+         if len(palavras_sorteadas) == 4:
+            print(historico_palavras_tentadas[i][0], historico_palavras_tentadas[i][1], historico_palavras_tentadas[i][2], historico_palavras_tentadas[i][3])
       
-   
-         for i in range(len(historico_palavras_tentadas)):
-            if len(palavras_sorteadas) == 4:
-               print(historico_palavras_tentadas[i][0], historico_palavras_tentadas[i][1],  historico_palavras_tentadas[i][2], historico_palavras_tentadas[i][3])
-            elif len(palavras_sorteadas) == 2:
-               print(historico_palavras_tentadas[i][0], historico_palavras_tentadas[i][1])
-            else:
-               print(historico_palavras_tentadas[i][0])
-         round +=1
+         elif len(palavras_sorteadas) == 2:
+            print(historico_palavras_tentadas[i][0], historico_palavras_tentadas[i][1])
 
+         else:
+            print(historico_palavras_tentadas[i][0])
+      round += 1
+      
+      if len(palavras_sorteadas) == 4 and round == 8 or len(palavras_sorteadas) == 2 and round == 6 or len(palavras_sorteadas) == 1 and round == 5:
+         print("[LOSS] VOCÊ PERDEU!")
+         confirm = input("::PRECIONE ENTER PARA VOLTAR AO MENU::")
+         break
+      
+      if len(palavras_sorteadas) == acertos.count(True):
+         print("[WIN] VOCÊ VENCEU O JOGO!")
+         confirm = input("::PRECIONE ENTER PARA VOLTAR AO MENU::")
+         break
