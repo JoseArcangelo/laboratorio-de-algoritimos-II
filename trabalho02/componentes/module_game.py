035fd422d3fe699b960a34a8aa3b551a82ffe6ea
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-11-19 23:02:35 -0300

    Update module_game.py

--- trabalho02/componentes/module_game.py
+++ trabalho02/componentes/module_game.py
@@ -7,44 +7,55 @@ def sortear_palavras(total_words):
    folder_words_usadas = open('arquivos/words_usadas.txt', 'r')
    list_words_usadas = folder_words_usadas.readlines()
    list_words = folder_words.readlines()
+   folder_words.close()
+   folder_words_usadas.close()
    palavras_sorteadas = []
+
+   if (len(list_words) - len(list_words_usadas)) < total_words:
+      return False
+   
    while True:
       word = list_words[random.randint(0, len(list_words) - 1)].upper()
+      
       if word not in list_words_usadas:
          word = word.replace("\n", "")
-         palavras_sorteadas.append(word)
+         
+         if word not in palavras_sorteadas and len(word) == 5:
+            palavras_sorteadas.append(word)
          
       if len(palavras_sorteadas) == total_words:
          break
-   folder_words.close()
-   folder_words_usadas.close()
+
    return palavras_sorteadas
 
-def input_word_tentativa():
+def input_word_tentativa(words_usadas):
    while True:
       try:
          word = input("\nInforme uma palavra: ").upper()
          if len(word) != 5:
             raise NameError("[ERRO] VOCÊ INFORMOU UMA PALAVRA QUE NÃO POSSUI 5 LETRAS! ")
          if not word.isalpha():
-            raise NameError("[ERRO] NÃO É PERMITIDO NÚMEROS!")
+            raise NameError("[ERRO] NÃO É PERMITIDO NÚMEROS NEM ESPAÇOS EM BRANCO!")
+         if word in words_usadas:
+            raise NameError("VOCÊ JÁ UTILIZOU ESTA PALAVRA!")
          else:
-            return word
+            words_usadas.append(word)
+            return word, words_usadas
       except ValueError:
          print("[ERRO] VOCÊ INFORMOU UMA PALAVRA INVÁLIDA!")
       except NameError as e:
          print(e)
 
-def colorir_palavras(round, letras_verdes, letras_amarelas, historico_palavras_tentadas, palavra, palavra_tentativa, letra , palavras_sorteadas):
+def colorir_palavras(round, letras_verdes, letras_amarelas, historico_palavras_tentadas, palavra, palavra_tentativa , palavras_sorteadas):
+   lista_letras_verdes = list(letras_verdes.values())
+   lista_indices_verdes = list(letras_verdes.keys()) 
    for i in range(5):
-      lista_indice_letras_verdes = list(letras_verdes.values())
-      lista_indices = list(letras_verdes.keys()) 
-      
-      if i in lista_indices:
+
+      if i in lista_indices_verdes:
          colored_letter = f"\033[32m{palavra_tentativa[i]}\033[0m"
          historico_palavras_tentadas[round][palavra] = f"{historico_palavras_tentadas[round][palavra]}{colored_letter} | "
       
-      elif i in letras_amarelas and lista_indice_letras_verdes.count(palavra_tentativa[i]) != palavras_sorteadas[palavra].count(palavra_tentativa[i]):
+      elif i in letras_amarelas and lista_letras_verdes.count(palavra_tentativa[i]) != palavras_sorteadas[palavra].count(palavra_tentativa[i]):
          colored_letter = f"\033[33m{palavra_tentativa[i]}\033[0m"
          historico_palavras_tentadas[round][palavra] = f"{historico_palavras_tentadas[round][palavra]}{colored_letter} | "
      
@@ -55,22 +66,24 @@ def colorir_palavras(round, letras_verdes, letras_amarelas, historico_palavras_t
 def game(total_words):
    ##Sortear as palavras, verificando se alguma ja foi utilizada
    palavras_sorteadas = sortear_palavras(total_words)
+   if palavras_sorteadas == False:
+      print("QUANTIDADE DE PALAVRAS INSUFICIENTES!")
+      return
    
    ##Criando um modo de conferir se o jogador acertou alguma palavra, juntamente com um contador e uma lista para guardar as palavras tentadas
    historico_palavras_tentadas = []
    round = 0
    acertos = []
+   words_usadas = []
    for i in range(total_words):
       acertos.append(False)
-
-
-   ###Tentativas do jogador, conferindo se as letras das palavras
+      
+  ###Tentativas do jogador, conferindo se as letras das palavras
    while True:
       print(palavras_sorteadas)
       historico_palavras_tentadas.append([])
-      palavra_tentativa = input_word_tentativa()
+      palavra_tentativa, words_usadas = input_word_tentativa(words_usadas)
       module_utilits.clear_terminal()
-      
       
       for palavra in range(len(palavras_sorteadas)):
          historico_palavras_tentadas[round].append("")
@@ -89,7 +102,7 @@ def game(total_words):
                if palavra_tentativa == palavras_sorteadas[palavra]:
                   acertos[palavra] = True
          
-            historico_palavras_tentadas = colorir_palavras(round, letras_verdes, letras_amarelas, historico_palavras_tentadas, palavra, palavra_tentativa, letra, palavras_sorteadas)
+            historico_palavras_tentadas = colorir_palavras(round, letras_verdes, letras_amarelas, historico_palavras_tentadas, palavra, palavra_tentativa, palavras_sorteadas)
       
       for i in range(len(historico_palavras_tentadas)):
          if len(palavras_sorteadas) == 4:
@@ -101,7 +114,7 @@ def game(total_words):
          else:
             print(historico_palavras_tentadas[i][0])
       round += 1
-      
+
       if len(palavras_sorteadas) == 4 and round == 8 or len(palavras_sorteadas) == 2 and round == 6 or len(palavras_sorteadas) == 1 and round == 5:
          print("[LOSS] VOCÊ PERDEU!")
          confirm = input("::PRECIONE ENTER PARA VOLTAR AO MENU::")
@@ -110,4 +123,9 @@ def game(total_words):
       if len(palavras_sorteadas) == acertos.count(True):
          print("[WIN] VOCÊ VENCEU O JOGO!")
          confirm = input("::PRECIONE ENTER PARA VOLTAR AO MENU::")
+         folder_words_usadas = open('arquivos/words_usadas.txt', 'a')
+         for word_usada in palavras_sorteadas:
+            folder_words_usadas.write(word_usada)
+            folder_words_usadas.write("\n")
+         folder_words_usadas.close()
          break
