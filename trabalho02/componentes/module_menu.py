20f769c7a5cea44e7a99d655841c0b463035ec08
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-11-21 16:07:53 -0300

    Update module_menu.py

--- trabalho02/componentes/module_menu.py
+++ trabalho02/componentes/module_menu.py
@@ -1,8 +1,12 @@
 def menu():
     """Menu de opções do jogo"""
-    print("\n1 - UMA PALAVRA")
-    print("2 - DUETO")
-    print("3 - QUARTETO")
-    print("4 - SAIR")
+    print("\n::BEM-VINDO AO JOGO TERMO!::")
+    print("DESAFIE-SE A ADIVINHAR PALAVRAS DE 5 LETRAS")
+    print("VOCÊ TERÁ FEEDBACK APÓS CADA TENTATIVA")
+    print("\n1 - NODO DE ADIVINHAÇÃO DE UMA PALAVRA")
+    print("2 - MODO DUETO")
+    print("3 - MODO QUARTETO")
+    print("4 - REDEFINIR PALAVRAS JÁ USADAS")
+    print("5 - SAIR")
     option = input("INFORME UMA OPÇÃO DE JOGO: ")
     return option
