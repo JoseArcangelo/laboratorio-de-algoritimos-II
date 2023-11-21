265e58a8018b5e5f25b69dbf3627bb9447eebbf8
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2023-11-21 16:07:37 -0300

    Update module_main.py

--- trabalho02/componentes/module_main.py
+++ trabalho02/componentes/module_main.py
@@ -1,11 +1,11 @@
 import module_menu
 import module_utilits
 import module_game
+import module_reset_words_used
 
 def main():
-    """Função principal do código que chama os outros módulos"""
-    print(":::BEM VINDO AO JOGO TERMO!:::")
-    while True: 
+   """Função principal do código que chama os outros módulos"""
+   while True: 
       option = module_menu.menu()
       if option == "1":
          module_utilits.clear_terminal()
@@ -19,7 +19,13 @@ def main():
          module_game.game(4)
 
       elif option == "4":
+         module_utilits.clear_terminal()
+         module_reset_words_used.reset_wrods_used()
+
+      elif option == "5":
          print("SAINDO...")
          break
-         
+      
+      else:
+         print("::OPÇÃO INVÁLIDA!::")
 main()  
